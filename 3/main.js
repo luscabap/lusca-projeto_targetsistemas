966b823cf7c72042db7b714126485546a314c818
Lucas Baptista <110420518+luscabap@users.noreply.github.com>
2023-03-30 17:38:54 -0300

    Add files via upload

--- 3/main.js
+++ 3/main.js
@@ -7,28 +7,25 @@ const campoTexto = document.getElementById('campoNumero')
 
 requisitarAPI().then(x => {
     const lista = x.filter(num => num.valor !== 0);
-    console.log(lista);
 
     const ValoresOrdenados = lista.sort((a, b)=> a.valor - b.valor);
-    console.log("O menor valor está no dia " + ValoresOrdenados[0].dia + " equivalente a " + ValoresOrdenados[0].valor);
-    console.log("O maior valor está no dia " + ValoresOrdenados[20].dia + " equivalente a " + ValoresOrdenados[20].valor)
+    const menorValor = ValoresOrdenados[0].dia;
 
     const listaNumeros = lista.map((x, y) => x.valor);
-    console.log(listaNumeros);
+
     const numerosSomados = listaNumeros.reduce((x, y)=> x + y);
-    console.log(numerosSomados.toFixed(2));
+
     const mediaMensal = numerosSomados / listaNumeros.length;
-    console.log(mediaMensal.toFixed(2));
 
     const numerosSuperioresMedia = listaNumeros.filter((x, y)=>{
         return x > mediaMensal;
     })
-    console.log(numerosSuperioresMedia);
 
     const diasCertos = numerosSuperioresMedia.length;
-    console.log(diasCertos);
 
     campoTexto.innerHTML = `
-        O número de dias no mês em que o valor de faturamento diário é superior à média mensal são: <strong class="mauz">${diasCertos} dias</strong>.
+        <p>O menor valor está no dia ${ValoresOrdenados[0].dia} equivalente a R$ ${ValoresOrdenados[0].valor.toFixed(2)}.</p>
+        <p>O maior valor está no dia ${ValoresOrdenados[20].dia} equivalente a R$ ${ValoresOrdenados[20].valor.toFixed(2)}.</p>
+        <p>O número de dias no mês em que o valor de faturamento diário é superior à média mensal são: <strong class="mauz">${diasCertos} dias</strong>.</p>
     `
 })
